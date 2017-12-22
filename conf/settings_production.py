6a58fe24cdb1b587f54ae852c4e2410321ef693f
weisc <weisc@seagroup.com>
2017-12-22 14:16:40 +0800

    first add

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -19,10 +19,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': 'blueKing_demo',                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'weisc',                            # 你的数据库user
-        'PASSWORD': '123456',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
+        'NAME': 'sicongwei',                        # 数据库名 (默认与APP_ID相同)
+        'USER': 'root',                            # 你的数据库user
+        'PASSWORD': 'bk@321',                        # 你的数据库password
+        'HOST': '10.0.1.220',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
