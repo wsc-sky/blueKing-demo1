36ae30961cdc983f24dba06d89678c2698423e38
weisc <weisc@seagroup.com>
2018-01-04 16:15:07 +0800

    first add

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -20,8 +20,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': 'blueking_weisc',                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'weisc',                            # 你的数据库user
-        'PASSWORD': 'jCXBQk2bmdWtRvfe',                        # 你的数据库password
+        'USER': 'root',                            # 你的数据库user
+        'PASSWORD': 'Garena.com@123',                        # 你的数据库password
         'HOST': '10.10.32.233',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
